a8562cf3a986137cc9c7f91a57265df0ce35e84a
Finlay Hudson <11finnh@gmail.com>
2024-09-22 21:09:25 +0100

    Intial commit: setup repo and basic routes

---
+++ src/ErrorPage.tsx
@@ -0,0 +1,28 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+function ErrorPage() {
+    const error = useRouteError();
+
+    let errorMessage: string;
+    if (isRouteErrorResponse(error)) {
+        errorMessage = error.statusText
+    } else if (error instanceof Error) {
+        errorMessage = error.message
+    } else if (typeof error === 'string') {
+        errorMessage = error
+    } else {
+        errorMessage = 'Unknown error';
+    }
+
+    return (
+        <div>
+            <h1>Oops</h1>
+            <p>Sorry an unexpected error occoured</p>
+            <p>
+                <i>{errorMessage}</i>
+            </p>
+        </div>
+    )
+}
+
+export default ErrorPage;
---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ErrorPage from './ErrorPage.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />
+  },
+])
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
