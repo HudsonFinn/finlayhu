cf7618ffc100cdbe1537d6f1fddd9b382f825069
Finlay Hudson <11finnh@gmail.com>
2024-09-28 20:02:05 +0100

    Add new tab page layout

--- src/pages/NewTabPage.tsx
+++ src/pages/NewTabPage.tsx
@@ -5,8 +5,32 @@ function NewTabPage() {
 	return (
 		<div className="page">
 			<h1>NewTab</h1>
-			<p>Base page for new tab idea</p>
+			<hr />
 			<QuoteOfTheDay />
+			<hr />
+			<div style={{ display: 'flex', flexWrap: 'wrap' }}>
+				<div
+					style={{
+						flexGrow: 1,
+						flexBasis: '400px',
+						margin: '10px 20px',
+					}}
+				>
+					<h3>Links</h3>
+					<p>This is where links will go</p>
+				</div>
+				<hr />
+				<div
+					style={{
+						flexGrow: 1,
+						flexBasis: '400px',
+						margin: '10px 20px',
+					}}
+				>
+					<h3>Information</h3>
+					<p>This is where information will go</p>
+				</div>
+			</div>
 		</div>
 	);
 }
