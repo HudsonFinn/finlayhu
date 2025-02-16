30641be8ae4477fb35f4b742dacff9a3346948b3
HudsonFinn <11finnh@gmail.com>
2025-02-16 01:30:56 +0000

    Add styling for the dark/lightmode toggle

--- README.md
+++ README.md
@@ -5,3 +5,16 @@
 -   Pull the repo
 -   npm install
 -   npm run dev
+
+### TODO:
+
+-   [x] Add styling for the darkmode toggle
+-   [ ] Add ability to darkmode preference to local storage
+-   [ ] Add way to automatically sync the articles published with my local notes
+    -   [ ] Setup articles to be served from S3
+    -   [ ] Vault will fetch avaliable articles and serve them to the user
+-   [ ] Add login
+    -   [ ] Expose specific endpoints only to logged in users
+        -   [ ] Personal LLM
+        -   [ ] Personal notes
+-   [ ] Setup email list
--- src/components/ToggleSwitch/ToggleSwitch.tsx
+++ src/components/ToggleSwitch/ToggleSwitch.tsx
@@ -1,3 +1,5 @@
+import './ToggleSwitch.css';
+
 type ToggleSwitchProps = {
 	checked: boolean;
 	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -6,7 +8,23 @@ type ToggleSwitchProps = {
 function ToggleSwitch(props: ToggleSwitchProps) {
 	const { checked, onChange } = props;
 	return (
-		<input type="checkbox" checked={checked} onChange={onChange}></input>
+		<div className="toggle-switch">
+			<input
+				type="checkbox"
+				id="switch-input"
+				checked={checked}
+				onChange={onChange}
+			></input>
+			<label htmlFor="switch-input" className="slider">
+				<img
+					className="gear-image"
+					src="/public/f44562c2-ba6f-4103-abfd-3413d15ce6e7-removebg-preview.png"
+					alt="gear"
+				/>
+				<img className="sun-image" src="/public/sun.png" alt="sun" />
+				<img className="moon-image" src="/public/moon.png" alt="moon" />
+			</label>
+		</div>
 	);
 }
 
