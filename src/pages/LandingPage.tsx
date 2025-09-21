4a65bdb507874f5c6ce4fed8470f496f090eb00c
HudsonFinn <11finnh@gmail.com>
2025-09-21 18:46:09 +0100

    Add tailwind, breaks some stuff

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,22 @@
+import { Link } from 'react-router-dom';
 import '../App.css';
 import useTypewriter from '../utilities/useTypewriter';
 
 function LandingPage() {
-	const text = useTypewriter(`I'm Finn. I write code.`, 100);
+	const text = useTypewriter(`I'm Finn. I write stuff.`, 100);
 	return (
 		<div className="page">
-			<div className="">
-				<h1>{text}</h1>
+			<div className="text-center">
+				<h1 className="text-3xl font-bold">{text}</h1>
+				<div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+					<Link
+						className="rounded-md bg-gray-700 px-3.5 py-2.5 text-sm font-semibold text-white inset-ring inset-ring-white/5"
+						to={'/vault'}
+					>
+						Vault
+					</Link>
+					<Link to={'/new-tab'}>Data</Link>
+				</div>
 			</div>
 		</div>
 	);
