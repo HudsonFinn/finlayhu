7c7082e02504f30481e8ae97cec44725a7a22e15
HudsonFinn <11finnh@gmail.com>
2025-12-08 16:12:38 +0000

    Update about page with contact information

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -3,11 +3,28 @@ import '../App.css';
 function AboutPage() {
 	return (
 		<div className="page">
-			<h1>About me</h1>
+			<h1 className="text-3xl text-bold">About me</h1>
 			<p>
-				This page is all about me. Contact me at{' '}
-				<a>https://www.linkedin.com/in/finn-hudson/</a>
+				Hey, I&apos;m Finn a Software Engineer who likes to write and
+				build stuff. You can contact me below.
 			</p>
+			<h2 className="text-2xl">Contact</h2>
+			<ul>
+				<li>
+					Linkedin:
+					<a href="https://www.linkedin.com/in/finn-hudson/">
+						finn-hudson
+					</a>
+				</li>
+				<li>
+					Email:
+					<a href="matilo:11finnh@gmail.com">11finnh@gmail.com</a>
+				</li>
+				<li>
+					Github:
+					<a href="https://www.github.com/HudsonFinn">HudsonFinn</a>
+				</li>
+			</ul>
 		</div>
 	);
 }
